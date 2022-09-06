f971c90e4ff18d1b9f96b02527ba6b9e25f37a61
Gooey Code <gooeycode@gmail.com>
2022-09-06 22:22:02 +0100

    adding in tests

--- blog/tests.py
+++ blog/tests.py
@@ -23,21 +23,21 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.author.username, "testuser")
         self.assertEqual(str(self.post), "A good title")
 
-    def test_url_exists_at_correct_location_listview(self): # new
+    def test_url_exists_at_correct_location_listview(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_exists_at_correct_location_detailview(self): # new
+    def test_url_exists_at_correct_location_detailview(self): 
         response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
 
-    def test_post_listview(self): # new
+    def test_post_listview(self): 
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Nice body content")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_post_detailview(self): # new
+    def test_post_detailview(self):
         response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
         no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
